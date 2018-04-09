7e36fdaeb92fd6011366a26060114e2ce79fab52
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-04-09 16:58:11 +0200

    change

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -2,33 +2,26 @@ var ContactForm = React.createClass({
   propTypes: {
     contact: React.PropTypes.object.isRequired
   },
-
   render: function() {
     return (
-      <form className = {'contactForm'} >
-      <input  type = {'text'}  placeholder = {'Imię'}>
-          value: this.props.contact.firstName,
-      </input>
-        }),
-        
-     
-      <input  type = {'text'}  placeholder = {'Nazwisko'}>
-          value: this.props.contact.lastName,
-      </input>
-        }),
-    
-   
-      <input  type = {'email'}  placeholder = {'Email'}>
-          value: this.props.contact.email,
-      </input>
-        }),
-       
-        
-        
-        
-       <button type = {'submit'} "Dodaj kontakt" />
-</form>
-      )
-    )
-  },
-})
+      <form className={"contactForm"}>
+        <input
+          type="text"
+          placeholder="Imię"
+          value={this.props.contact.firstName}
+        />
+        <input
+          type="text"
+          placeholder="Nazwisko"
+          value={this.props.contact.lastName}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={this.props.contact.email}
+        />
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+});
